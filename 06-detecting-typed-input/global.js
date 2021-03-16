89557480ce1596fb6f8f5da1cfc6f8d9ead78297
Markuss <markusspriede@inbox.lv>
2021-03-16 15:49:05 +0200

    6th lesson

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,10 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const name_field = document.getElementById("full_name");
+  
+    name_field.addEventListener("keyup", function() {
+    const greeting_div = document.getElementById("greeting");
+    greeting_div.innerHTML = ("Hello there, " + name_field.value + "!");
+  });
+
+});
